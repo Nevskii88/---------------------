85ee4a83542d1d874cfcadd486432a6bcfae49aa
Juliy <tima1002100@mail.ru>
2023-04-02 14:40:30 +0300

    one commit

---
+++ HomeWork №3/1.js
@@ -0,0 +1,3 @@
+const cel = Number.parseInt(prompt ("Введите температуру в градусах Цельсия: "))
+const far = Number.parseFloat(9/5 * cel + 32)
+alert (`Цельсий: ${cel} Фаренгейт: ${(far).toFixed(1)}`)
